3ba7c745f8386c6619fa7e0165f40f8a61de3fa3
shyam <shyamzod03@gmail.com>
2024-04-23 01:04:53 +0530

    js file changes

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -6,7 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const ws_1 = require("ws");
 const app = (0, express_1.default)();
-const httpserver = app.listen(3000);
+app.get("/home", (req, res) => {
+    res.send("This is the basic get route from server!!!");
+});
+const httpserver = app.listen(8080);
 const wss = new ws_1.WebSocketServer({ server: httpserver });
 wss.on("connection", function connection(ws) {
     ws.on("error", console.error);
